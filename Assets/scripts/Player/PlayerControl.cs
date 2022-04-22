69d40a2147b27020cc51a95860caf6732540d18a
mcalle1@eafit.edu.co <mcalle1@eafit.edu.co>
2022-04-21 19:37:11 -0500

    fix: returning to old player controller

--- Assets/scripts/Player/PlayerControl.cs
+++ Assets/scripts/Player/PlayerControl.cs
@@ -5,7 +5,6 @@ using TMPro;
 public class PlayerControl : Player
 {
     public bool isMoveable;
-    private bool isMoving = false;
 
     [SyncVar]
     public float speed;
@@ -22,7 +21,7 @@ public class PlayerControl : Player
         if (hasAuthority)
         {
             Camera cam = Camera.main;
-            if(cam != null)
+            if (cam != null)
             {
                 cam.transform.SetParent(transform);
                 cam.transform.localPosition = new Vector3(0f, 0f, -10f);
@@ -43,28 +42,26 @@ public class PlayerControl : Player
 
     private void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        bool isMoving = false;
         if (hasAuthority && isMoveable)
         {
             dir = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f), 1f);
             transform.position += dir * speed * Time.deltaTime;
             isMoving = dir.magnitude != 0f;
         }
-    }
 
-    private void Update()
-    {
-        Move();
-    }
-
-    private void Move()
-    {
-
-        if(isMoving && (hasAuthority && isMoveable))
+        if (isMoving)
         {
             animator.SetFloat("moveX", dir.x);
             animator.SetFloat("moveY", dir.y);
             animator.SetBool("moving", true);
-        } else
+        }
+        else
         {
             animator.SetBool("moving", false);
         }
