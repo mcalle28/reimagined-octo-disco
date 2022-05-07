937a115792ee0bfff4b0e8da9dd7ff022d04bf7f
mcalle1@eafit.edu.co <mcalle1@eafit.edu.co>
2022-05-07 08:19:20 -0500

    enh: removing returning button from already finished game, now after 10 secs it returns to room

--- Assets/Scripts/In-game Logic/Countdown.cs
+++ Assets/Scripts/In-game Logic/Countdown.cs
@@ -5,13 +5,15 @@ public class Countdown : NetworkBehaviour
 {
     public double timeLeft = 180;
     public TMP_Text timerText;
-    public bool IsRunning = false;
-    private double sinceStarted = 0;
+    public bool IsRunning, BackToRoom;
+    private double sinceStarted, totalTime;
 
     private void Start()
     {
         IsRunning = true;
+        BackToRoom = false;
         sinceStarted= NetworkTime.time;
+        totalTime = timeLeft;
     }
 
     void Update()
@@ -20,14 +22,27 @@ public class Countdown : NetworkBehaviour
         {
             if (timeLeft > 0)
             {
-               timeLeft = 180-(NetworkTime.time-sinceStarted);
+               timeLeft = totalTime-(NetworkTime.time-sinceStarted);
                DisplayTime(timeLeft);
             }
             else
             {
                 timeLeft = 0;
                 IsRunning = false;
+                BackToRoom = true;
                 if(isServer) GameSystem.Instance.RpcCheckGhostWinCon();
+            }
+        }
+        else if (BackToRoom)
+        {
+            if(timeLeft > - 10)
+            {
+                timeLeft = 0 - (NetworkTime.time - sinceStarted - totalTime);
+            }
+            else
+            {
+                ChangeToRoom();
+                BackToRoom = false;
             }
         }
 
@@ -38,6 +53,12 @@ public class Countdown : NetworkBehaviour
         double minutes = (int)(timeToDisplay / 60);
         double seconds = (int)(timeToDisplay % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void ChangeToRoom()
+    {
+        var manager = NetworkManager.singleton as GHNetworkManager;
+        if (isServer) manager.ServerChangeScene(manager.RoomScene);
     }
 }
 
